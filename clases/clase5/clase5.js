3d8a9845d270904e2c2c88505227a219705841c1
ezequiel villegas <ezevillegas0@gmail.com>
2022-12-27 18:19:10 -0300

    clase 5

---
+++ clases/clase5/clase5.js
@@ -0,0 +1,13 @@
+// CLASE 4, Administrador de Paquetes - NPM
+
+             //ADMINISTRADORES DE PAQUETES
+
+// revision del desafio 2
+// 00:27:24 comienza la clase - importar/exportar modulos
+// 00:38:02 continua la clase npm
+// 00:51:34 actividad en clase
+// 01:17:00 npm - package.json
+// 01:23:16 instalar dependencias npm
+// 01:29:13 packagelock json
+// 01:39:42 scripts - nodemon
+// 01:52:47 operadores para actulizar veriones
